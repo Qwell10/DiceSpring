ba897de53eb9af63e57acb2e6d7ea8e1fbdb72b6
QwellPrace <michaloupka29@gmail.com>
2026-04-14 07:51:40 +0200

    used SessionConnectEvent instead of SessionConnectedEvent

--- src/main/java/com/dice/listener/WebSocketEventListener.java
+++ src/main/java/com/dice/listener/WebSocketEventListener.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
@@ -15,7 +15,7 @@ public class WebSocketEventListener {
     private RegistrationService registrationService;
 
     @EventListener
-    public void handleConnect(SessionConnectedEvent event) {
+    public void handleConnect(SessionConnectEvent event) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
 
         String playerIdStr = accessor.getFirstNativeHeader("playerId");
